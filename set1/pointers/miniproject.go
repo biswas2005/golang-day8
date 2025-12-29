0b9fb7474c539edaf19648247fb7468bea78c9d3
biswas2005 <biswas2005a@gmail.com>
2025-12-29 20:17:41 +0530

    Modified Bankaccount actions

--- set1/pointers/miniproject.go
+++ set1/pointers/miniproject.go
@@ -7,31 +7,74 @@ type BankAccount struct {
 	Balance float64
 }
 
-func (a *BankAccount) Deposit(amount float64) {
+//Deposit() adds an amount to Bank Account
+func (a *BankAccount) Deposit() {
+
+	var amount float64
+	fmt.Println("\nEnter an amount to Deposit:")
+	fmt.Scan(&amount)
+
 	if amount > 0 {
 		fmt.Printf("\nAmount of %.3f has been deposited to %s's account.", amount, a.Name)
+		updatedBalance := a.Balance + amount
+		fmt.Printf("\nUpdated Balance:%f", updatedBalance)
+	} else {
+		fmt.Print("\nEnter valid amount.")
 	}
 
 }
 
-func (a *BankAccount) Withdraw(amount float64) {
+//withdraw() an amount from account
+func (a *BankAccount) Withdraw() {
+
+	var amount float64
+	fmt.Println("\nEnter an amount to withdraw:")
+	fmt.Scan(&amount)
 	if amount > 0 && amount <= a.Balance {
 		fmt.Printf("\n%.2f has be deducted from %s's account.", amount, a.Name)
+		updatedBalance := a.Balance - amount
+		fmt.Printf("\nUpdated Balance:%f", updatedBalance)
 	} else {
 		fmt.Println("Insufficient Balance.")
 		fmt.Printf("\nAvailable balance %.3f", a.Balance)
 	}
 }
 
+//Display() the Account details
 func (a *BankAccount) Display() {
 	fmt.Printf("Account Holder:%s Balance:%.3f", a.Name, a.Balance)
 }
 
 func BankAcc() {
-	account := BankAccount{Name: "Biswas", Balance: 2000.0}
+	for {
+		var n int
+		//prints a menu to choose action
+		fmt.Println("\n<Choose an option>")
+		fmt.Println("1.Diposit Money.")
+		fmt.Println("2.Withdraw Money.")
+		fmt.Println("3.Dispaly Details.")
+		fmt.Println("4.EXIT.")
+		fmt.Println("Enter your choice:")
+		fmt.Scan(&n)
+		account := BankAccount{Name: "Biswas\n", Balance: 2000.0}
 
-	account.Display()
+		//switch() between cases
+		switch n {
+		case 1:
+			account.Deposit()
+		case 2:
+			account.Withdraw()
+		case 3:
 
-	account.Deposit(500)
-	account.Withdraw(1000)
+			account.Display()
+		case 4:
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Println("Invalid Choice!")
+
+		}
+
+	}
+
 }
